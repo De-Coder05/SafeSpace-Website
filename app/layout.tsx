fbe1ca8f141a0fc5e3873e874371ccfa15ad2e77
Arsh Chatrath <142995090+arshchatrath@users.noreply.github.com>
2025-07-11 23:09:10 +0530

    added chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,11 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Navbar from "./components/Navbar"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "SafeSpace AI - Mental Health Empowered by AI",
   description: "Detect stress through voice, sensors, and psychology with SafeSpace AI",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -19,6 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script async={false} src="https://cdn.botpress.cloud/webchat/v3.0/inject.js"></script>
+      <script async={false} src="https://files.bpcontent.cloud/2025/07/11/16/20250711160316-693TLH0U.js"></script>
       <body className={inter.className}>
         <Navbar />
         {children}
